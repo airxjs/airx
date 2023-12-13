49480b0dc85cb68c897b2b68489f01c29700eb71
Alain <yinxulai@hotmail.com>
2023-12-13 21:06:22 +0800

    Support ref nesting (#13)
    * Support ref nesting
    
    * remove isRefObject

--- source/reactive.ts
+++ source/reactive.ts
@@ -45,12 +45,6 @@ function createRefObject<T = unknown>(value: T): Ref<T> {
   return object
 }
 
-function isRefObject<T = unknown>(obj: unknown): obj is Ref<T> {
-  return obj != null
-    && typeof obj === 'object'
-    && Reflect.has(obj, symbol.airxReactiveDependenciesSymbol)
-}
-
 export function watch<T = unknown>(ref: Ref<T>, listener: () => unknown) {
   const deps: Set<() => unknown> = Reflect.get(ref, symbol.airxReactiveDependenciesSymbol)
   deps.add(listener)
@@ -58,7 +52,7 @@ export function watch<T = unknown>(ref: Ref<T>, listener: () => unknown) {
 }
 
 export function createRef<T>(obj: T): Ref<T> {
-  const ref = isRefObject<T>(obj) ? obj : createRefObject(obj)
+  const ref = createRefObject(obj)
 
   if (!globalContext.dependencies.has(ref)) {
     globalContext.dependencies.add(ref)
