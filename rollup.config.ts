cc4f67a4deb6e7a26695d50228efff2ced533c9f
Alain <yinxulai@hotmail.com>
2024-04-17 15:58:24 +0800

    Upgrade dev deps & add package bunder (#17)
    * add renderToHTML api
    
    * generate umd bundler
    
    * add esm export
    
    * update dev deps

---
+++ rollup.config.ts
@@ -0,0 +1,41 @@
+import typescript from 'rollup-plugin-typescript2'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { babel } from '@rollup/plugin-babel'
+import pkg from './package.json'
+
+
+const baseOutputOptions = {
+	sourcemap: true,
+	name: pkg.name
+}
+
+const umdOutputOptions = {
+	...baseOutputOptions,
+	file: pkg.main,
+	format: 'umd',
+	name: pkg.name
+}
+
+const moduleOutputOptions = {
+	...baseOutputOptions,
+	file: pkg.module,
+	format: 'module',
+	name: pkg.name
+}
+
+export default [
+	{
+		input: 'source/index.ts',
+		plugins: [
+			resolve({ browser: true }),
+			commonjs(),
+			typescript(),
+			babel({ babelHelpers: 'bundled' })
+		],
+		output: [
+			umdOutputOptions,
+			moduleOutputOptions,
+		]
+	}
+]
